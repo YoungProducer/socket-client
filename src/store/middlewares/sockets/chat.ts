8c7432be3184c2acafcae826433434c488d277a1
YoungProducer <sashabezrukovownmail@gmail.com>
2020-05-08 23:24:41 +0300

    feat: added callback to chatMiddleware on success join.

--- src/store/middlewares/sockets/chat.ts
+++ src/store/middlewares/sockets/chat.ts
@@ -1,6 +1,7 @@
 import { Middleware, PayloadAction } from '@reduxjs/toolkit';
 import { Socket } from 'components/MessagesWatcher/Socket';
-import { Chat } from 'store/slices/chat';
+import { Chat, setChatListAction } from 'store/slices/chat';
+import { RootState } from 'store/slices';
 
 const socket = new Socket();
 
@@ -10,5 +11,18 @@ export const chatMiddleware: Middleware =
 
         if (action.type === 'Chat/joinAction') {
             socket.join(action.payload);
+
+            socket.instance.on('join-response', (data: { status: string }) => {
+                if (data.status === 'Success!') {
+                    const state: RootState = store.getState();
+
+                    socket.chatList(state.chatList.userId);
+
+                    socket.instance.on('chat-list-response', (data: Chat[]) => {
+                        store.dispatch(setChatListAction(data));
+                    });
+                }
+            });
+
         }
     };
